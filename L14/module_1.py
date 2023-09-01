c547b1344fb8e266a4720ae1e9c8057906110bf2
notyasin <67812297+notyasin@users.noreply.github.com>
2023-09-01 17:14:01 +0900

    Add files via upload

---
+++ L14/module_1.py
@@ -0,0 +1,27 @@
+def add_(x, y):
+    total = x + y
+    return total
+
+
+def subtract_(x, y):
+    total = x - y
+#     print("test")
+    return total
+
+
+def myname():
+    print("yasin")
+    print("asumi")
+    print("xuan")
+    print("austin")
+    
+def random_number_generator(x):
+    import random
+    list1 = []
+    for i in range(x):
+        gg = random.randint(0, 99)
+        list1.append(gg)
+        
+    return list1
+    
+    
